f4d53c9b4ab600bf1d40f4f20771f7b3472fecd5
Aditya Bavadekar <adityarbavadekar@gmail.com>
2022-10-24 14:45:51 +0530

    Usage of Library files and classes.

--- mvn-sample-app/src/main/java/com/adityaamolbavadekar/mvn_sample_app/MainActivity.kt
+++ mvn-sample-app/src/main/java/com/adityaamolbavadekar/mvn_sample_app/MainActivity.kt
@@ -3,13 +3,27 @@ package com.adityaamolbavadekar.mvn_sample_app
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
-import com.adityaamolbavadekar.mvn_sample.MVNSampleJavaClass
+import io.github.adityabavadekar.mvn_sample.InstanceId
+import io.github.adityabavadekar.mvn_sample.MVNSample
+import io.github.adityabavadekar.mvn_sample.java.MVNSampleJavaClass
+import io.github.adityabavadekar.mvn_sample.kotlin.MVNSampleKotlinClass
+import io.github.adityabavadekar.mvn_sample.logger.Log
 
 class MainActivity : Activity() {
 
+    private val logger = Log.TAG(javaClass.simpleName)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, MVNSampleJavaClass.instance().sayHello(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, MVNSampleJavaClass.getInstance().sayHello(), Toast.LENGTH_SHORT).show()
+        logger.d("Hi")
+        logger.d("Hello")
+        logger.d("MVN-Sample App")
+        MVNSample {
+            "String"
+        }
+        InstanceId.getInstance().id
+        MVNSampleKotlinClass.instance.sayHello()
     }
 
 }
